439a1b4cfaff5cf329304d1941f4e5ac96296f00
SDTheSlayer <dalal.shivang@gmail.com>
2019-04-27 21:02:38 +0530

    Login both

--- Login/post_login.php
+++ Login/post_login.php
@@ -3,11 +3,13 @@
 include("config.php");
 	$username = $_POST['username'];
 	$password = $_POST['password'];
+	$type = $_POST['type'];
 
-	 $username = stripcslashes($username);
-	 $password = stripcslashes($password);
-	 $username = mysqli_real_escape_string($db,$username);
-	 $password = mysqli_real_escape_string($db,$password);
+	$username = stripcslashes($username);
+	$password = stripcslashes($password);
+	$username = mysqli_real_escape_string($db,$username);
+	$password = mysqli_real_escape_string($db,$password);
+	$type = mysqli_real_escape_string($db,$type);
 
 
 	$password = md5($password);
@@ -16,11 +18,11 @@ include("config.php");
 	// mysqli_connect("localhost","root","");
 	// mysqli_select_db("mess_rating");
 
-	$result = mysqli_query($db,"SELECT * FROM students WHERE rollno = '$username' AND password = '$password'") or die("Failed".mysqli_error());
+	$result = mysqli_query($db,"SELECT * FROM $type WHERE username = '$username' AND password = '$password'") or die("Failed".mysqli_error($db));
 
 	$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
 
-	if ($row['rollno'] == $username && $row['password'] == $password){
+	if ($row['username'] == $username && $row['password'] == $password){
 		echo "Login Success wlecome ".$row['name'];
 	}
 	else{
